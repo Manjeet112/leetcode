f808adff526813dac7eea4aa0f0e484fc18769e5
Manjeet112 <manjeetkaur404363@gmail.com>
2026-06-02 11:21:05 +0530

    Time: 16 ms (69.34%) | Memory: 36.1 MB (99.29%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int maxlen=0;
+        for(int j=0;j<nums.size();j++){
+            while(nums[j]-nums[i]>1){
+                i++;
+            }
+            if(nums[j]-nums[i]==1){
+                maxlen=max(maxlen,j-i+1);
+            }
+        }
+        return maxlen;
+    }
+};
